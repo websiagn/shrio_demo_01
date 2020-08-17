3d44ca2e42f35f1da13064838161a11472bae335
youchen <1450207968@qq.com>
2020-08-17 17:00:01 +0800

    初始化Demo，引入依赖

---
+++ src/main/java/com/shrio/shrio_demo_01/ShrioDemo01Application.java
@@ -0,0 +1,13 @@
+package com.shrio.shrio_demo_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrioDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShrioDemo01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/shrio/shrio_demo_01/ShrioDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrio.shrio_demo_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrioDemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
